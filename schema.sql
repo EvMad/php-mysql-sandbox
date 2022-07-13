7558e0bf74f9940470e9468fc9048582ad784de6
GitPit <rifflifter@gmail.com>
2022-07-13 12:57:31 -0400

    CREATE TABLE users()

--- schema.sql
+++ schema.sql
@@ -1,3 +1,10 @@
 DROP DATABASE IF EXISTS collection_db;
 CREATE DATABASE collection_db;
 USE collection_db;
+
+CREATE TABLE users(
+    id VARCHAR(30) NOT NULL,
+    full_name VARCHAR(250) DEFAULT NULL,
+    email VARCHAR(250) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
