5099be5cca9cb24d70fda27e89c7474be8249c98
GitPit <rifflifter@gmail.com>
2022-07-13 13:04:49 -0400

    <body><h1><h2><form>

--- index.php
+++ index.php
@@ -5,3 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Registration Form</title>
 </head>
+
+
+<body>
+<h1>Sign Up</h1>
+    <div><h2>Registration Form</h2></div>
+    <form action='connect.php' method="POST">
+</body>
